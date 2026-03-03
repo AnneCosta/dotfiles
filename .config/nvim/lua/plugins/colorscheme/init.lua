bb105c945eb703ad00001293103e04e008e8d3f0
Anne Costa <fabiannecsilva@gmail.com>
2026-03-03 17:09:14 -0300

    chore: ajustar cores do tema Everforest do Neovim

--- .config/nvim/lua/plugins/colorscheme/init.lua
+++ .config/nvim/lua/plugins/colorscheme/init.lua
@@ -10,14 +10,52 @@ return {
 
         background = "hard",
         background_hardness = "medium",
-        colours_override = function(palette)
-          palette.fg = "#8fbf90"
+        on_highlights = function(hl, palette)
+          hl.SnacksDashboardKey = { fg = "#c5ede5", bg = palette.none }
+          hl.SnacksDashboardIcon = { fg = "#c5ede5", bg = palette.none }
+          hl.SnacksDashboardHeader = { fg = "#83c092" }
+          hl.SnacksDashboardFooter = { fg = "#6eac80" }
+          hl.SnacksDashboardDesc = { fg = "#94D195" }
+          hl.SnacksPickerPathHidden = { fg = palette.grey1 }
+          hl.SnacksPickerGitStatusUntracked = { fg = palette.grey2 }
+          hl.SnacksDashboardSpecial = { fg = "#c5ede5" }
+          hl.NoiceLspProgressTitle = { fg = "#9ece6a" }
+          hl.LspInlayHint = { fg = palette.grey1 }
+          hl.NonText = { fg = "#636962" }
+          hl.LineNr = { fg = "#636962" }
+          hl.LineNrAbove = { fg = "#636962" }
+          hl.LineNrBelow = { fg = "#636962" }
         end,
       })
     end,
   },
 
-  -- { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        background = {
+          light = "mocha",
+          dark = "mocha",
+        },
+        float = {
+          transparent = true,
+          solid = false,
+        },
+        term_colors = true,
+        auto_integrations = true,
+        transparent_background = true,
+        flavour = "mocha",
+        color_overrides = {
+          all = {
+            peach = "#9ece6a",
+          },
+        },
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
